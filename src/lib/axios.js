72f139cbb3057e7a7a89c15aff9bf03b6305caef
shushant <shushantkumar0625@gmail.com>
2025-04-13 17:06:21 +0530

    change link

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api/",
+    baseURL: "https://chat-app-backend-8paw.onrender.com/api/",
    withCredentials:true,
 });
